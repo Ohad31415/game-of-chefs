d67a7606ec15bc4fb6c058140b665aa86d4ef104
Dana_Bezalel <danabez1@gmail.com>
2020-01-21 16:52:19 +0200

    Changes files to work with Master file - minor changes

--- recipies_start2end.py
+++ recipies_start2end.py
@@ -4,41 +4,42 @@ from scraper import get_all_recipe
 from recipes_scraping import scrape_recipe_bbcgoodfood
 import requests
 
+
 def parse_args():
     """the function gets user's argument of whether to search according to specific Key word or Category"""
-    category_opts = {'c&b': 'Cakes & baking', 'cu': 'Cuisines', 'ev': 'Events', 'hl': 'Healthy', 'ing': 'Ingredients',
-                     'oc': 'Occasions', 'q&e': 'Quick & easy', 'die': 'Special diets', 'veg': 'Vegetarian'}
+    category_opts = {'c_b': 'Cakes & baking', 'cu': 'Cuisines', 'ev': 'Events', 'hl': 'Healthy', 'ing': 'Ingredients',
+                     'oc': 'Occasions', 'q_e': 'Quick & easy', 'die': 'Special diets', 'veg': 'Vegetarian'}
     category_help = 'chose recipe category:' + str(category_opts)
-    parser = argparse.ArgumentParser(description='get argument to limit search: --key_word, --category')
-    parser.add_argument('--key_word', type=str, help='recipe search word(s)')
-    parser.add_argument('--category', choices=category_opts.keys(), type=str, help=category_help)
+    parser = argparse.ArgumentParser(description='get argument to limit search')
+    parser.add_argument('-q', '--query', type=str, help='recipe search word(s)')
+    parser.add_argument('-c', '--category', choices=category_opts.keys(), type=str, help=category_help)
 
     args = parser.parse_args()
     category_option = category_opts.get(args.category)
 
-    if category_option and args.key_word:
+    if category_option and args.query:
         raise ValueError('Only one argument can be given')
 
-    if not (category_option and args.key_word):
+    if not (category_option or args.query):
         raise ValueError('Please enter a search argument')
 
-    return category_option, args.key_word
+    return category_option, args.query
 
 
 def main():
     # gets user's arguments (if exist)
     try:
-        category_selection, key_word_selection = parse_args()
+        category_selection, query_selection = parse_args()
 
         # if a key_word was give, search for all corresponding recipes
-        if key_word_selection:
-            recipe_urls = search_results(query=key_word_selection)
+        if query_selection:
+            recipe_urls = search_results(query=query_selection)
         elif category_selection:
-            recipie_urls = get_all_recipe(category_selection)
+            recipe_urls = get_all_recipe(category_selection)
 
-        for url in recipie_urls:
+        for url in recipe_urls:
             recipe_content = scrape_recipe_bbcgoodfood(url)
-            #todo enter to database
+            # todo enter to database
 
     except ValueError as err:
         print(err)
